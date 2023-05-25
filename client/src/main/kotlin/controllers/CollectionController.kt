7de8286468bd22a16c5492f5efca737120922382
Kosty Manuhin <kostymanuhin@gmail.com>
2023-05-25 12:10:38 +0300

    collectionController
    -can get collection
    -clientController can execute command

---
+++ client/src/main/kotlin/controllers/CollectionController.kt
@@ -0,0 +1,30 @@
+package controllers
+
+import data.MusicBand
+import org.koin.core.component.inject
+import serialize.Serializer
+
+class CollectionController : ClientController() {
+    private val serializer: Serializer<LinkedHashMap<Int, MusicBand>> by inject<Serializer<LinkedHashMap<Int, MusicBand>>>()
+    private var collection: LinkedHashMap<Int, MusicBand> = LinkedHashMap()
+
+    fun getCollection() = collection.clone() as LinkedHashMap<*, *>
+
+    fun updateCollection(): Boolean {
+        when (val res = executeCommand("show", arrayOf())) {
+            is CommandResult.Success -> {
+                logger.info { "Коллекция получена!" }
+                collection =  serializer.deserialize(res.message!!)
+                return true
+            }
+            is CommandResult.Failure -> {
+                logger.info { "Ошибка при попытке получить коллекцию: ${res.throwable.message}" }
+                return false
+            }
+            null -> {
+                logger.info { "Не удалось получить коллекцию" }
+                return false
+            }
+        }
+    }
+}
